7b22e23e547aef6cb4fef95be6788efeeff8c05f
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-13 12:39:41 +1000

    setting things up

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const socket = require("socket.io");
+// App setup
+const app = express();
+const server = app.listen(4000, function () {
+  console.log("listening for requests on port 4000,");
+});
+
+// Static files
+app.use(express.static("public"));
+
+const io = socket(server);
+io.on("connection", (socket) => {
+  console.log("made socket connection", socket.id);
+});
