8f2ede900a6c88c9cefafa03278a9102bd7110d7
Stanley Huang <infamous_godhand@yahoo.com>
2020-05-14 11:52:08 +1000

    update for socket emit

--- app.js
+++ app.js
@@ -12,4 +12,8 @@ app.use(express.static("public"));
 const io = socket(server);
 io.on("connection", (socket) => {
   console.log("made socket connection", socket.id);
+  socket.on("chat", function (data) {
+    // console.log(data);
+    io.sockets.emit("chat", data);
+  });
 });
